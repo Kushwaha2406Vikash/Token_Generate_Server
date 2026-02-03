348a7d5c9e42ee3007313744df762c35c680afbb
Vikash Kushwaha <136932881+Kushwaha2406Vikash@users.noreply.github.com>
2026-02-04 00:21:48 +0530

    Fix baseURL assignment in apihandler.js add the render url

--- frontend/src/apihandler.js
+++ frontend/src/apihandler.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000" || import.meta.env.VITE_API_URL, // FastAPI backend
+  baseURL: import.meta.env.VITE_API_URL, // FastAPI backend
   headers: {
     "Content-Type": "application/json",
   },
